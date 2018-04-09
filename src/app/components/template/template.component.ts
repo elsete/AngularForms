f3cbc0a1cede564bb683bff86d5424571cce4e27
Sete <elsete79@gmail.com>
2018-04-09 17:16:48 +0200

    160. Uso de Select, Radio y Checkbox

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -12,11 +12,21 @@ import { NgForm } from '@angular/forms';
 })
 export class TemplateComponent implements OnInit {
 
-  public usuario = {
+  public usuario: Object = {
     nombre: null,
     apellido: null,
-    correo: null
+    correo: null,
+    pais: '',
+    sexo: 'Hombre'
   };
+
+  public paises = [{
+    codigo: 'CRI',
+    nombre: 'Costa Rica'
+  }, {
+    codigo: 'ESP',
+    nombre: 'España'
+  }];
 
   constructor() {
 
